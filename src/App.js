df7c0d1b8d3e9596e4b050ab0d2d6bcad69991c7
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-06-18 18:20:54 +0530

    chapter 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Video from './components/Video';
+import videos from './data/data'
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Videos</div>
+      {
+        videos.map(video=><Video
+          key={video.id}
+          title={video.title}
+          views={video.views}
+          time={video.time}
+          channel={video.channel}
+          verified={video.verified}
+          id={video.id}
+        ></Video>)
+      }
+    
     </div>
   );
 }
